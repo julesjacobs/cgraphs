c5b379fb18de12f3d104f998bb27e7678b2b5bbb
julesjacobs <julesjacobs@gmail.com>
2020-03-02 11:40:53 +0100

    test

--- theories/test.v
+++ theories/test.v
@@ -1,5 +1,56 @@
 From iris.proofmode Require Import tactics.
 
+Inductive af {X} (R : X -> X -> Prop) : Prop :=
+  | full : (forall x y : X, R x y) -> af R
+  | tail : (forall x, af (fun y z => R y z \/ R x y)) -> af R.
+
+Definition eqb (a b : bool) :=
+  match a,b with
+  | true,true | false,false => True
+  | true,false | false,true => False
+  end.
+
+Lemma af_eqb : af eqb.
+Proof.
+  apply tail. intros a.
+  apply tail. intros b.
+  apply full. intros c.
+  destruct a,b,c; simpl; auto.
+Qed.
+
+Ltac dodest :=
+  match goal with
+  | x : _ |- _ => destruct x; simpl
+  end.
+
+Hint Extern 100 => dodest : destruct.
+
+Lemma af_inf_chain X (R : X -> X -> Prop) :
+  af R -> forall (f : nat → X), ∃ m, ∃ n, R (f m) (f n) /\ (m < n).
+Proof.
+  induction 1; intros; [|destruct (H0 (f 0) (f ∘ S))];
+  naive_solver (eauto with lia).
+  Unshelve. exact 0.
+Qed.
+
+Lemma af_impl {X} (R R' : X -> X -> Prop) :
+  (forall x y, R x y -> R' x y) -> af R -> af R'.
+Proof.
+  intros Hi Hr. revert R' Hi. induction Hr;
+  eauto 10 using full,tail with destruct.
+Qed.
+
+Lemma le_af : af le.
+Proof.
+  apply tail. intros n. induction n.
+  - apply full. lia.
+  - apply tail. intros m. destruct m.
+    + apply full. lia.
+    + apply tail. intros k.
+      eapply af_impl; last done. simpl.
+      intros x y []; lia.
+Qed.
+
 Ltac dorew :=
   match goal with
   | x : _ |- _ => (rewrite x; clear x) || (rewrite -x; clear x)
@@ -14,12 +65,17 @@ Definition plus (n m : cnat) : cnat := fun _ f x => n _ f (m _ f x).
 Definition mult (n m : cnat) : cnat := fun _ f => n _ (m _ f).
 Definition exp (n m : cnat) : cnat := fun _ => m _ (n _).
 
+(* Alternative definitions based on function composition: *)
+
 Definition succ' (n : cnat) : cnat := fun _ f => f ∘ n _ f.
 Definition plus' (n m : cnat) : cnat := fun _ f => n _ f ∘ m _ f.
+Definition mult' (n m : cnat) : cnat := fun _ => n _ ∘ m _.
+Definition exp' (n m : cnat) : cnat := fun _ => m _ (n _).
 
 Definition one := succ zero.
 Definition two := succ one.
 Definition three := succ two.
+Definition four := succ three.
 
 Example ex1 : plus' zero zero = zero. reflexivity. Qed.
 Example ex2 : plus' zero one = one. reflexivity. Qed.
@@ -29,6 +85,9 @@ Example ex5 : plus' two zero = two. reflexivity. Qed.
 Example ex6 : plus' zero two = two. reflexivity. Qed.
 Example ex7 : plus' one two = three. reflexivity. Qed.
 Example ex8 : plus' two one = three. reflexivity. Qed.
+Example ex9 : mult' two two = four. reflexivity. Qed.
+Example ex10 : mult' two one = two. reflexivity. Qed.
+
 
 Ltac doind :=
   match goal with
@@ -72,7 +131,7 @@ Qed.
 Definition cZ : cnat := fun t f z => z.
 Definition cS (n : cnat) : cnat := fun t f z => f (n t f z).
 
-Definition p2_nat f g := 
+Definition p2_nat f g :=
   forall a b, forall R : a -> b -> Prop,
   forall x y s r, R x y -> (forall x' y', R x' y' -> R (s x') (r y')) -> R (f a x s) (g b y r).
 
