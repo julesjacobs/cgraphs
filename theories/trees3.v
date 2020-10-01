d379a6a453e6765c2f82d2016b21deed0dd611cf
julesjacobs <julesjacobs@gmail.com>
2020-10-01 12:13:51 +0200

    Update trees3.v

--- theories/trees3.v
+++ theories/trees3.v
@@ -1,5 +1,4 @@
 From stdpp Require Import gmap.
-From stdpp Require Import base.
 
 Definition lookup_mod `{Inhabited A} (i : Z) (xs : list A) : A :=
   xs !!! (Z.to_nat (i `mod` length xs)).
@@ -112,7 +111,7 @@ Section graph.
   Lemma graph_singleton_elem (x y a b : A) :
     (a,b) ∈ graph_singleton x y <-> (a = x ∧ b = y) ∨ (a = y) ∧ (b = x).
   Proof. unfold graph_singleton. set_solver. Qed.
-  Lemma graph_singleton_sym (x y : A) : 
+  Lemma graph_singleton_sym (x y : A) :
     graph_singleton x y = graph_singleton y x.
   Proof. unfold graph_singleton. set_solver. Qed.
   Lemma graph_singleton_elem_1 (x y z : A) :
@@ -166,7 +165,7 @@ Section graph.
     unfold lookup_edge in *.
     set_solver by eauto using elem_of_list_lookup_mod_r with lia.
   Qed.
-  
+
   Lemma tree_extend (x y : A) (g : graph A) :
     x ≠ y → lone y g →
     tree g → tree (g ∪ graph_singleton x y).
@@ -198,7 +197,7 @@ Section graph.
   Qed.
 
   Lemma dec_exists_list {B} (xs : list B) (P : B → Prop) :
-  (∀ x, P x → x ∈ xs) → (∀ x, x ∈ xs → Decision (P x)) → Decision (∃ x, P x).
+    (∀ x, P x → x ∈ xs) → (∀ x, x ∈ xs → Decision (P x)) → Decision (∃ x, P x).
   Proof.
     intros HPxs Hdec.
     induction xs as [|x xs IHxs]. { right. set_solver. }
@@ -251,8 +250,36 @@ Section graph.
     lia.
   Qed.
 
+  Lemma mod_0_k (n i k : Z) :
+    (n > 0)%Z ->
+    ((i - k) `mod` n = 0 -> 0 ≤ k -> k < n -> i `mod` n = k)%Z.
+  Proof.
+    intros.
+    erewrite <-Zmod_unique_full. done. unfold Remainder. lia.
+    rewrite Zmod_eq in H2;[|done].
+    assert (i = n * ((i - k) `div` n) + k)%Z. lia. done.
+  Qed.
+
+  Lemma mod_horrible (i n : Z) :
+    (0 < n)%Z ->
+    (i `mod` n = n - Z.succ ((- i - 1) `mod` n))%Z.
+  Proof.
+    intro.
+    pose proof (Z.mod_pos_bound (- i - 1) n)%Z.
+    eapply mod_0_k; try lia.
+    assert (∀ x, Z.succ x = x + 1)%Z as Q by lia.
+    rewrite Q.
+    rewrite <- Zminus_mod_idemp_r.
+    rewrite <- (Zminus_mod_idemp_r n).
+    rewrite Zplus_mod_idemp_l.
+    rewrite (Zminus_mod_idemp_r n).
+    rewrite Zminus_mod_idemp_r.
+    replace (i - (n - (- i - 1 + 1)))%Z with ((-1)*n)%Z by lia.
+    rewrite Z.mod_mul; lia.
+  Qed.
+
   Lemma lookup_mod_rev (i : Z) (xs : list A) :
-    lookup_mod i xs = lookup_mod (- i - 1) (rev xs).
+    lookup_mod i xs = lookup_mod (- i - 1)%Z (rev xs).
   Proof.
     destruct (decide (length xs = 0)).
     {
@@ -266,22 +293,26 @@ Section graph.
     rewrite lookup_reverse.
     {
       subst. rewrite<- !list_lookup_total_alt. f_equiv.
-      revert n.
-      generalize (length xs). intros.
-      rewrite<- Z2Nat.inj_succ;[|apply Z_mod_pos; lia].
-      replace n with (Z.to_nat (Z.of_nat n));[|lia].
-      rewrite<- Z2Nat.inj_sub.
-      2: {
-        assert (0 <= (- i - 1) `mod` Z.to_nat n)%Z.
-        apply Z_mod_pos. lia. lia.
-      }
-      f_equiv.
-      assert (n = 37) by admit.
-      assert (i = 163) by admit.
-      subst. done.
+      replace (length xs) with (Z.to_nat (Z.of_nat (length xs))) by lia.
+      assert (Z.of_nat (length xs) > 0)%Z by lia.
+      clear n. revert H1.
+      generalize (Z.of_nat (length xs)). intros n Hn.
+      rewrite Z2Nat.id;[|lia].
+      pose proof (Z.mod_pos_bound (-i-1) n)%Z.
+      assert (S (Z.to_nat ((- i - 1) `mod` n)) ≤ Z.to_nat n) by lia.
+      rewrite <-Z2Nat.inj_succ by lia.
+      rewrite <-Z2Nat.inj_sub by lia.
+      f_equiv. apply mod_horrible. lia.
     }
     destruct (Z.mod_pos_bound (- i - 1) (length xs)). lia. lia.
-  Admitted.
+  Qed.
+
+  Lemma lookup_mod_rev' (i : Z) (xs : list A) :
+    lookup_mod (- i - 1) xs = lookup_mod i (rev xs).
+  Proof.
+    replace i with (-(-i - 1) -1)%Z by lia.
+    rewrite<- lookup_mod_rev. f_equiv. lia.
+  Qed.
 
   Lemma cycle_rev (g : graph A) (xs : list A) :
     undirected g ->
@@ -293,13 +324,12 @@ Section graph.
     - apply NoDup_rev. done.
     - intros i.
       unfold lookup_edge in *.
-(*       
-      rewrite<- lookup_mod_rev.
+      rewrite<- !lookup_mod_rev'.
       replace (- (i + 1) - 1)%Z with (- i - 2)%Z by lia.
       replace (-i - 1)%Z with ((-i - 2)+1)%Z by lia.
       apply Hundir.
       apply cycle_cycle0.
-  Qed. *)
+  Qed.
 
   Lemma cycle_mono (g h : graph A) (xs : list A) :
     cycle g xs -> cycle (g ∪ h) xs.
@@ -330,44 +360,120 @@ Section graph.
     naive_solver.
   Qed.
 
-  Definition delete_mod (i : Z) (xs : list A) : list A.
-  Admitted.
-  
-  Definition insert_mod (i : Z) (x : A) (xs : list A) : list A.
-  Admitted.
+  Fixpoint insert {T} (i : nat) (x : T) (xs : list T) : list T :=
+      match i,xs with
+      | 0,xs => x :: xs
+      | S i',y :: xs => y :: insert i' x xs
+      | S i',[] => []
+      end.
 
-  Lemma insert_mod_length (i : Z) (x : A) (xs : list A) :
-    length (insert_mod i x xs) = 1 + length xs.
-  Proof. Admitted.
+  Lemma insert_length {T} (i : nat) (x : T) (xs : list T) :
+    i ≤ length xs -> length (insert i x xs) = length xs + 1.
+  Proof.
+    revert i. induction xs; intros; destruct i; simpl in *; auto with lia.
+  Qed.
 
-  Lemma delete_mod_length (i : Z) (xs : list A) :
+  Lemma in_insert {T} (i : nat) (x a : T) (xs : list T) :
+    i ≤ length xs -> (a ∈ insert i x xs <-> a = x ∨ a ∈ xs).
+  Proof.
+    revert i; induction xs; intros; destruct i; simpl in *; rewrite ?elem_of_cons, ?IHxs;
+    rewrite ?elem_of_cons; naive_solver lia.
+  Qed.
+
+  Lemma insert_NoDup {T} (i : nat) (x : T) (xs : list T) :
+    i ≤ length xs ->
+      (x ∉ xs ∧ NoDup xs <-> NoDup (insert i x xs)).
+  Proof.
+    revert i; induction xs; destruct i; simpl; intros;
+    rewrite ?list.NoDup_cons; eauto with lia.
+    rewrite <-IHxs, in_insert, not_elem_of_cons; naive_solver lia.
+  Qed.
+
+  Lemma insert_out_of_bounds {T} (i : nat) (x : T) (xs : list T) :
+    i > length xs -> insert i x xs = xs.
+  Proof.
+    revert i; induction xs; destruct i; simpl; intros; try f_equiv; eauto with lia.
+  Qed.
+
+  Lemma insert_NoDup_2 {T} (i : nat) (x : T) (xs : list T) :
+    x ∉ xs -> NoDup xs -> NoDup (insert i x xs).
+  Proof.
+    destruct (decide (i ≤ length xs)).
+    - rewrite <-insert_NoDup; eauto with lia.
+    - rewrite insert_out_of_bounds; eauto with lia.
+  Qed.
+
+  Lemma insert_delete {T} (i : nat) (x : T) (xs : list T) :
+    delete i (insert i x xs) = xs.
+  Proof.
+    revert i; induction xs; destruct i; simpl; try f_equiv; eauto.
+  Qed.
+
+  Lemma delete_insert {T} (i : nat) (x : T) (xs : list T) :
+    xs !! i = Some x -> insert i x (delete i xs) = xs.
+  Proof.
+    revert i; induction xs; destruct i; simpl; intros; try f_equiv; naive_solver.
+  Qed.
+
+  Definition insert_mod (i : Z) (x : A) (xs : list A) :=
+      insert (Z.to_nat (i `mod` length xs)) x xs.
+
+  Definition delete_mod (i : Z) (xs : list A) :=
+      delete (Z.to_nat (i `mod` length xs)) xs.
+
+  Lemma delete_mod_length i xs :
     length (delete_mod i xs) = length xs - 1.
-  Proof. Admitted.
+  Proof.
+    destruct (decide (length xs = 0)).
+    { by destruct xs; simplify_eq. }
+    unfold delete_mod.
+    rewrite length_delete. done.
+    by apply lookup_mod_some.
+  Qed.
 
-  Lemma insert_mod_NoDup (i : Z) (x : A) (xs : list A) :
-    x ∉ xs -> NoDup xs -> NoDup (insert_mod i x xs).
-  Proof. Admitted.
+  Lemma in_delete x i (xs : list A) :
+    x ∈ delete i xs -> x ∈ xs.
+  Proof.
+    revert i; induction xs; intros; destruct i; simpl in *;
+    eauto; rewrite elem_of_cons; eauto.
+    rewrite elem_of_cons in H1.
+    destruct H1; eauto.
+  Qed.
 
-  Lemma delete_mod_NoDup (i : Z) (xs : list A) :
+  Lemma NoDup_delete i (xs : list A) :
+    NoDup xs -> NoDup (delete i xs).
+  Proof.
+    intro. revert i; induction xs; intros; destruct i; simpl in *; eauto.
+    - eapply NoDup_cons_12. done.
+    - eapply NoDup_cons_2; eauto using NoDup_cons_12.
+      rewrite list.NoDup_cons in H1. intro.
+      apply in_delete in H2. naive_solver.
+  Qed.
+
+  Lemma NoDup_delete_mod i xs :
     NoDup xs -> NoDup (delete_mod i xs).
-  Proof. Admitted.
+  Proof.
+    intro. unfold delete_mod.
+    apply NoDup_delete. done.
+  Qed.
 
-  Definition edges (xs : list A) : gset (A * A). Admitted.
+  Lemma insert_mod_length i x (xs : list A) :
+    length xs > 0 -> length (insert_mod i x xs) = length xs + 1.
+  Proof.
+    unfold insert_mod. intro.
+    rewrite insert_length. done.
+    pose proof (Zmod_pos_bound i (length xs)).
+    naive_solver lia.
+  Qed.
 
-  Lemma edges_delete (xs : list A) (a b c : A) (i : nat) :
-    3 ≤ length xs ->
-    lookup_mod i xs = a ->
-    lookup_mod (i+1) xs = b ->
-    lookup_mod (i+2) xs = c ->
-    edges (delete_mod (i+1) xs) = edges xs ∖ {[a,b]} ∖ {[b,c]} ∪ {[a,c]}.
-  Proof. Admitted.
-
-  Lemma edges_insert (xs : list A) (a b c : A) (i : nat) :
-    3 ≤ length xs ->
-    lookup_mod i xs = a ->
-    lookup_mod (i+1) xs = c ->
-    edges (insert_mod (i+1) b xs) = edges xs ∖ {[a,c]} ∪ {[a,b]} ∪ {[b,c]}.
-  Proof. Admitted.
+  Lemma insert_mod_NoDup i x (xs : list A) :
+    length xs > 0 -> x ∉ xs -> NoDup xs -> NoDup (insert_mod i x xs).
+  Proof.
+    intros. unfold insert_mod.
+    apply insert_NoDup; eauto.
+    pose proof (Zmod_pos_bound i (length xs)).
+    naive_solver lia.
+  Qed.
 
   Lemma lookup_edge_mod (i : Z) (xs : list A) :
     length xs ≠ 0 ->
@@ -388,7 +494,7 @@ Section graph.
     - f_equiv. rewrite rev_involutive. done.
   Qed.
 
-  Definition swap {A} (p : A * A) := 
+  Definition swap {A} (p : A * A) :=
       let (a,b) := p in (b,a).
 
   Lemma lookup_edge_reverse (i : Z) (xs : list A) :
@@ -405,13 +511,13 @@ Section graph.
     ¬ cycle (g ∖ graph_singleton x z ∪ graph_singleton y z) xs.
   Proof.
     intros Hxneqz Hyneqz [] Hxyg Hxzg Hcycle.
-    assert (Decision (∃ i, 
-      lookup_edge i xs = (y,z) ∨ 
+    assert (Decision (∃ i,
+      lookup_edge i xs = (y,z) ∨
       lookup_edge i xs = (z,y))).
     {
       assert (Decision (∃ i,
-        0 <= i ∧ i < length xs ∧ 
-        (lookup_edge i xs = (y,z) ∨ 
+        0 <= i ∧ i < length xs ∧
+        (lookup_edge i xs = (y,z) ∨
         lookup_edge i xs = (z,y))))%Z.
       {
         apply (dec_exists_list (map Z.of_nat (seq 0 (length xs))))%Z.
@@ -433,7 +539,7 @@ Section graph.
         rewrite lookup_edge_mod;[|lia].
         done.
     }
-    
+
     destruct H1 as [Hedge|Hnotedge].
     2: {
       apply (tree_no_cycle0 xs).
@@ -448,10 +554,10 @@ Section graph.
     cut (∀ (xs : list A) (x y z : A) (i : Z),
       lookup_edge i xs = (z, y)
       → cycle (g ∖ graph_singleton x z ∪ graph_singleton y z) xs
-        → (x, z) ∈ g
-          → (x, y) ∈ g
-            → (∀ xs0 : list A, ¬ cycle g xs0)
-              → no_self_loops g → undirected g → x ≠ z → y ≠ z → False).
+      → (x, z) ∈ g
+      → (x, y) ∈ g
+      → (∀ xs0 : list A, ¬ cycle g xs0)
+      → no_self_loops g → undirected g → x ≠ z → y ≠ z → False).
     {
       intros.
       destruct Hedge; eauto.
@@ -461,11 +567,26 @@ Section graph.
       simpl. unfold lookup_edge in H2. simplify_eq. done.
       apply cycle_rev; eauto.
       apply undirected_extend.
-      apply undirected_delete. 
+      apply undirected_delete.
       done.
     }
-    intros xs x y z.
-    intros i Hedge Hcycle Hxzg Hxyg tree_no_cycle0 tree_no_self_loops0
+
+    cut (∀ (xs : list A) (x y z : A) (i : Z),
+      x ∉ xs
+      → lookup_edge i xs = (z, y)
+      → cycle (g ∖ graph_singleton x z ∪ graph_singleton y z) xs
+      → (x, z) ∈ g
+      → (x, y) ∈ g
+      → (∀ xs0 : list A, ¬ cycle g xs0)
+      → no_self_loops g → undirected g → x ≠ z → y ≠ z → False).
+    {
+      intros.
+      destruct (decide (x ∈ xs)) as [Hin|]; eauto.
+
+      admit.
+    }
+    intros xs x y z i.
+    intros Hxnotinxs Hedge Hcycle Hxzg Hxyg tree_no_cycle0 tree_no_self_loops0
             tree_undirected0 Hxneqz Hyneqz.
     destruct Hcycle.
 
@@ -490,16 +611,13 @@ Section graph.
         * rewrite graph_singleton_elem in *.
           assert (x = y). naive_solver.
           eapply tree_no_self_loops0. subst. eauto.
-      + apply delete_mod_NoDup. done.
+      + apply NoDup_delete_mod. done.
       + intros j.
         admit.
     - apply (tree_no_cycle0 (insert_mod (i+1) x xs)).
       constructor.
-      + rewrite insert_mod_length. lia.
-      + apply insert_mod_NoDup; [|done]. intro.
-        (* if x is in xs, we can construct a smaller cycle *)
-        
-        admit.
+      + rewrite insert_mod_length; lia.
+      + apply insert_mod_NoDup; eauto with lia.
       + intros j.
         (* prove that after inserting x, the cycle is in g *)
         admit.
@@ -513,30 +631,173 @@ Section graph.
     intros ?? [] ??.
     constructor.
     - apply undirected_extend. apply undirected_delete. done.
-    - apply no_self_loops_extend;[done|]. 
+    - apply no_self_loops_extend;[done|].
       apply no_self_loops_delete;done.
-    - intros. eapply no_cycle_modify; done. 
+    - intros. eapply no_cycle_modify; done.
   Qed.
 
   Lemma tree_delete (x y : A) (g : graph A) :
     tree g -> tree (g ∖ graph_singleton x y).
   Proof.
+    intros []. constructor.
+    - intros ???.
+      rewrite elem_of_difference in *.
+      split.
+      + apply tree_undirected0. naive_solver.
+      + unfold graph_singleton in *. set_solver.
+    - intros ??.
+      rewrite elem_of_difference in H1.
+      eapply tree_no_self_loops0. naive_solver.
+    - intros. intro. apply (tree_no_cycle0 xs).
+      destruct H1.
+      constructor; eauto with lia.
+      intro. specialize (cycle_cycle0 i).
+      rewrite elem_of_difference in cycle_cycle0.
+      naive_solver.
+  Qed.
 
-  Admitted.
+  Definition vertices (g : graph A) : gset A :=
+    set_map fst g ∪ set_map snd g.
 
-  Definition cardinality_at_most (n : nat) (g : graph A) :=
-    ∃ V : list A,
-      length V = n ∧
-      ∀ x y, (x,y) ∈ g -> x ∈ V ∧ y ∈ V.
+  Definition cardinality (g : graph A) : nat := size (vertices g).
+
+  Definition no_u_turns (f : A -> option A) : Prop :=
+      ∀ a b c, f a = Some b -> f b = Some c -> a ≠ c.
+
+  (* Inductive Vertex :=
+    | Thread : nat -> Vertex
+    | Channel : nat -> Vertex. *)
+
+  Fixpoint search_iter
+    (g : graph A) (f : A -> option A) (a : A) (n : nat) : A :=
+    match n with
+    | 0 => a
+    | S n => match f a with
+             | None => a
+             | Some a' => search_iter g f a' n
+             end
+    end.
+
+  Definition search (g : graph A) (x : A) (f : A -> option A) : A :=
+    search_iter g f x (cardinality g).
+
+  (*
+
+                                      C5
+                                      |
+  T1 -- C1 -- T2 -- C2 -- T3 -- C3 -- T4
+
+  B -- T1 -- C1 -- T2
+
+  T1 -- C1L -- C1R -- T2
+
+              C1
+              |
+  B1 -- T1 -- C2 -- T2
+
+  f(Thread n) =
+    if n can take a step
+    then None
+    else Some (Channel that n is blocked on)
+
+  f(Channel k) =
+    if the left endpoint of k is held by a thread and the thread is blocked on k
+    then Some (the channel/thread holding the right endpoint)
+    else Some (the channel/thread holding the left endpoint)
+
+  f(Channel (k,b)) =
+    if we are held by a thread and the thread is blocked on us
+    then Some(Channel (k, negb b)))
+    else Some(the channel/thread holding us)
+
+  T1 = recv C1
+  T2 = recv C2
+  T3 = 1+3; ...
+
+  C1 = ([],[])
+  C2 = ([],[]) *)
+
+  Definition path (g : graph A) (xs : list A) :=
+      ∀ i a b, xs !! i = Some a -> xs !! (i+1) = Some b -> (a,b) ∈ g.
 
   Definition no_u_turn (xs : list A) :=
-    ∀ i x y, xs !! i = Some x -> xs !! (i+2) = Some y -> x ≠ y.
+    ∀ i a b, xs !! i = Some a -> xs !! (i+2) = Some b -> a ≠ b.
 
-  Lemma no_u_turn_tree_paths_are_short (g : graph A) (xs : list A) (n : nat) :
-    cardinality_at_most n g ->
-    tree g -> path g xs -> no_u_turn xs ->
-    length xs <= n.
+  Lemma long_path_has_duplicate_vertex (s : gset A) (xs : list A) :
+    (∀ i x, xs !! i = Some x -> x ∈ s) ->
+    size s < length xs ->
+    ∃ i j a, xs !! i = Some a ∧ xs !! j = Some a ∧ i ≠ j.
+  Proof.
+    revert s. induction xs as [|x xs IH]; simpl; [lia|].
+    intros s Hxs Hsize.
+    destruct (decide (Exists (x=.) xs)) as [(? & Hx & <-)%Exists_exists|H2].
+    - apply elem_of_list_lookup in Hx as [j Hj].
+      exists 0,(S j),x.
+      eauto with lia.
+    - apply not_Exists_Forall in H2; [|solve_decision].
+      rewrite Forall_forall in H2. simpl in *.
+      assert (x ∈ s).
+      { specialize (Hxs 0). naive_solver. }
+      assert (∀ i x, xs !! i = Some x -> x ∈ s).
+      { intros i x' Hi. specialize (Hxs (S i)). naive_solver.  }
+      destruct (IH (s ∖ {[x]})) as (i & j & a & ?).
+      * intros i x' Hi.
+        apply elem_of_difference. split;[naive_solver|].
+        apply not_elem_of_singleton_2. apply not_symmetry.
+        apply H2. eapply elem_of_list_lookup_2. done.
+      * assert (size s = 1 + size (s ∖ {[x]})); try lia.
+        replace 1 with (size ((singleton x) : gset A)); try apply size_singleton.
+        replace (size s) with (size ({[x]} ∪ s)); try apply size_union_alt.
+        f_equiv. apply subseteq_union_1_L.
+        rewrite <-elem_of_subseteq_singleton. done.
+      * exists (S i),(S j),a. naive_solver.
+  Qed.
+
+  Lemma no_u_turn_drop (xs : list A) (k : nat) :
+    no_u_turn xs -> no_u_turn (drop k xs).
+  Proof.
+    intros Hnut i x y Hx Hy.
+    rewrite lookup_drop in Hx.
+    rewrite lookup_drop in Hy.
+    eapply (Hnut (k + i)). done.
+    replace (k + i + 2) with (k + (i + 2)) by lia. done.
+  Qed.
+
+  Lemma lookup_take_Some (xs : list A) (k i : nat) (x : A) :
+    take k xs !! i = Some x -> xs !! i = Some x.
+  Proof.
+    revert k i. induction xs; destruct k, i; naive_solver.
+  Qed.
+
+  Lemma no_u_turn_take (xs : list A) (k : nat) :
+    no_u_turn xs -> no_u_turn (take k xs).
+  Proof.
+    intros Hnut i x y Hx Hy.
+    eapply (Hnut i); eauto using lookup_take_Some.
+  Qed.
+
+  Lemma no_uturn_path_to_cycle (g : graph A) (xs : list A) :
+    (∃ i j a, xs !! i = Some a ∧ xs !! j = Some a ∧ i < j) ->
+    no_u_turn xs -> path g xs ->
+    ∃ xs' x, no_u_turn ([x] ++ xs' ++ [x]) ∧ path g ([x] ++ xs' ++ [x]).
+  Proof.
+    intros [i [j [a (Hi & Hj & Hij)]]] Hnut Hpath.
+    exists (take (j - i) (drop (i + 1) xs)), a.
+    split.
+    - intros k x y Hx Hy. admit.
+    - intros k x y Hx Hy. admit.
+  Admitted.
+
+  Lemma no_no_uturn_cycles (g : graph A) (xs : list A) (x : A) :
+    tree g -> path g ([x] ++ xs ++ [x]) ->
+    no_u_turn ([x] ++ xs ++ [x]) -> False.
   Proof.
   Admitted.
-  
-  (* set_wf *)
+
+  Definition valid (g : graph A) (f : A -> option A) :=
+    ∀ x y, x ∈ vertices g -> f x = Some y -> (x,y) ∈ g.
+
+  Lemma search_lemma (g : graph A) (x : A) (f : A -> option A) :
+    x ∈ vertices g -> tree g -> no_u_turns f -> valid g f ->
+    f (search g x f) = None.
+  Admitted.
