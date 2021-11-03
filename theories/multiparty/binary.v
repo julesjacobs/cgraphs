b8d56f9991c97bd6096e7721f1732d24b3106e49
julesjacobs <julesjacobs@gmail.com>
2021-11-03 14:55:49 +0100

    Update binary.v

--- theories/multiparty/binary.v
+++ theories/multiparty/binary.v
@@ -1,9 +1,9 @@
 From diris.multiparty Require Import langdef.
 
 Definition SendB e1 i e2 := Send 0 e1 i e2.
-Definition RecvB e1 := Recv 0 e1.
-Definition ForkB e1 := Spawn 1 (λ _, e1).
-Definition CloseB e1 := Close e1.
+Definition RecvB e := Recv 0 e.
+Definition ForkB e := Spawn 1 (const e).
+Definition CloseB e := Close e.
 
 CoInductive session_typeB :=
   | SendTB n : (fin n -> type) -> (fin n -> session_typeB) -> session_typeB
@@ -26,28 +26,59 @@ CoFixpoint toM (σ : session_typeB) : session_type :=
 
 Definition ChanTB σ := ChanT (toM σ).
 
-Lemma SendB_typed Γ1 Γ2 e1 e2 n t r i :
-  disj Γ1 Γ2 ->
-  typed Γ1 e1 (ChanTB (SendTB n t r)) ->
-  typed Γ2 e2 (t i) ->
-  typed (Γ1 ∪ Γ2) (SendB e1 (fin_to_nat i) e2) (ChanTB (r i)).
-Proof.
-Admitted.
+Section helpers.
+  (* Helper definitions and proofs for the binary typing rules. *)
+  Definition session_typeB_id (σ : session_typeB) : session_typeB :=
+    match σ with
+    | SendTB n ts σs => SendTB n ts σs
+    | RecvTB n ts σs => RecvTB n ts σs
+    | EndTB => EndTB
+    end.
 
-Lemma RecvB_typed Γ e n t r :
-  typed Γ e (ChanTB (RecvTB n t r)) ->
-  typed Γ (RecvB e) (SumNT n (λ i, PairT (ChanTB (r i)) (t i))).
+  Lemma session_typeB_id_id (σ : session_typeB) :
+    session_typeB_id σ = σ.
+  Proof.
+    by destruct σ.
+  Qed.
+
+End helpers.
+
+Lemma SendB_typed Γ1 Γ2 e1 e2 n ts σs i :
+  disj Γ1 Γ2 ->
+  typed Γ1 e1 (ChanTB (SendTB n ts σs)) ->
+  typed Γ2 e2 (ts i) ->
+  typed (Γ1 ∪ Γ2) (SendB e1 i e2) (ChanTB (σs i)).
 Proof.
-Admitted.
+  unfold ChanTB. intros.
+  eapply (Send_typed _ _ _ _ _ _ (toM ∘ σs)); eauto.
+  econstructor; last done.
+  constructor. apply session_type_equiv_alt. simpl. done.
+Qed.
+
+Lemma RecvB_typed Γ e n ts σs :
+  typed Γ e (ChanTB (RecvTB n ts σs)) ->
+  typed Γ (RecvB e) (SumNT n (λ i, PairT (ChanTB (σs i)) (ts i))).
+Proof.
+  unfold ChanTB. intros.
+  eapply (Recv_typed _ _ _ _ (toM ∘ σs)); eauto.
+  econstructor; last done.
+  constructor. apply session_type_equiv_alt. simpl. done.
+Qed.
 
 Lemma CloseB_typed Γ e :
   typed Γ e (ChanTB EndTB) ->
   typed Γ (CloseB e) UnitT.
 Proof.
-Admitted.
+  unfold ChanTB. intros.
+  constructor. econstructor; last done.
+  constructor. apply session_type_equiv_alt. simpl. done.
+Qed.
 
 Lemma ForkB_typed Γ ct e :
   typed Γ e (FunT (ChanTB (dual ct)) UnitT) ->
   typed Γ (ForkB e) (ChanTB ct).
 Proof.
+  unfold ChanTB, ForkB. intros.
+  About Spawn_typed.
+  econstructor.
 Admitted.
