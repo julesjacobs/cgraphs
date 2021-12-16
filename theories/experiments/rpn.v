5923ee435cc848b0a2ab141d931c323377cb8e1f
julesjacobs <julesjacobs@gmail.com>
2021-12-16 15:39:15 +0300

    Update rpn.v

--- theories/experiments/rpn.v
+++ theories/experiments/rpn.v
@@ -2,29 +2,29 @@ From iris Require Import bi.
 
 Inductive exp :=
   | Var (n : nat) | Lit (n : nat)
-  | BinOp (f : nat -> nat -> nat) (e1 e2 : exp).
+  | Op (f : nat -> nat -> nat) (e1 e2 : exp).
 
 Fixpoint eval v e :=
   match e with
   | Var n => v n | Lit n => n
-  | BinOp f e1 e2 => f (eval v e1) (eval v e2)
+  | Op f e1 e2 => f (eval v e1) (eval v e2)
   end.
 
-Inductive op := VarO (n : nat) | LitO (n : nat) | BinOpO (f : nat -> nat -> nat).
+Inductive op := VarR (n : nat) | LitR (n : nat) | OpR (f : nat -> nat -> nat).
 
 Definition o_eval v (s : option (list nat)) (o : op) :=
   match o,s with
-  | VarO n,Some s => Some (v n::s)
-  | LitO n,Some s => Some (n::s)
-  | BinOpO f, Some (n::m::s) => Some (f m n::s)
+  | VarR n,Some s => Some (v n::s)
+  | LitR n,Some s => Some (n::s)
+  | OpR f, Some (n::m::s) => Some (f m n::s)
   | _,_ => None
   end.
 Definition rpn_eval v s := foldl (o_eval v) (Some s).
 
 Fixpoint to_rpn (e : exp) :=
   match e with
-  | Var n => [VarO n] | Lit n => [LitO n]
-  | BinOp f e1 e2 => to_rpn e1 ++ to_rpn e2 ++ [BinOpO f]
+  | Var n => [VarR n] | Lit n => [LitR n]
+  | Op f e1 e2 => to_rpn e1 ++ to_rpn e2 ++ [OpR f]
   end.
 
 Lemma rpn_eval_to_rpn v s e ops :
