829668f9d78afcf7f6467e9ba14c74b27ba8004f
julesjacobs <julesjacobs@gmail.com>
2022-02-16 18:52:27 +0100

    MicroGV

--- theories/microgv/sessions.v
+++ theories/microgv/sessions.v
@@ -9,6 +9,28 @@ CoInductive session_type :=
   | RecvTB n : (fin n -> type) -> (fin n -> session_type) -> session_type
   | EndTB : session_type.
 
+
+Global Instance finvec_equiv `{Equiv T} n : Equiv (fin n -> T) := λ f g, ∀ i, f i ≡ g i.
+
+CoInductive session_type_equiv : Equiv session_type :=
+  | cteq_EndT : EndTB ≡ EndTB
+  | cteq_SendT n t1 t2 f1 f2 : t1 ≡ t2 -> f1 ≡ f2 -> SendTB n t1 f1 ≡ SendTB n t2 f2
+  | cteq_RecvT n t1 t2 f1 f2 : t1 ≡ t2 -> f1 ≡ f2 -> RecvTB n t1 f1 ≡ RecvTB n t2 f2.
+Global Existing Instance session_type_equiv.
+
+Axiom session_type_extensionality : ∀ σ1 σ2 : session_type, σ1 ≡ σ2 -> σ1 = σ2.
+
+(*
+  Coq's default notion of equality is not good enough for coinductive types:
+   the default equality is syntactic equality and not extensional equality.
+   We add an axiom to make equality extensional.
+   See https://coq.inria.fr/refman/language/core/coinductive.html:
+   "More generally, as in the case of positive coinductive types,
+   it is consistent to further identify extensional equality of coinductive
+   types with propositional equality"
+   Such an axiom is similar to functional extensionality, but for coinductive types.
+*)
+
 CoFixpoint dual (σ : session_type) : session_type :=
   match σ with
   | SendTB n ts σs => RecvTB n ts (dual ∘ σs)
@@ -16,14 +38,13 @@ CoFixpoint dual (σ : session_type) : session_type :=
   | EndBT => EndTB
   end.
 
-
 (* Interpretation of session types into linear lambda calculus types *)
 
 CoFixpoint toL (σ : session_type) : type :=
   match σ with
   | SendTB n ts σs => FunT Lin (SumT n (λ i, PairT (toL (dual $ σs i)) (ts i))) UnitT
   | RecvTB n ts σs => FunT Lin UnitT (SumT n (λ i, PairT (toL (σs i)) (ts i)))
-  | EndBT => UnitT
+  | EndBT => FunT Lin UnitT UnitT
   end.
 
 Definition session_type_id (σ : session_type) : session_type :=
@@ -66,7 +87,7 @@ Definition SendB e1 i e2 :=
       (Fork (Fun "z" (App (Var "x") (Sum i (Pair (Var "z") (Var "y")))))))).
 Definition RecvB e := App e Unit.
 Definition ForkB e := Fork e.
-Definition CloseB (e : expr) := e.
+Definition CloseB (e : expr) := App e Unit.
 
 Lemma env_split_left Γ :
   env_split Γ Γ ∅.
@@ -75,11 +96,46 @@ Proof.
   unfold disj. intros ???. smap.
 Qed.
 
+Definition toL1 (σ : session_type) : type :=
+  match σ with
+  | SendTB n ts σs => SumT n (λ i, PairT (toL (dual $ σs i)) (ts i))
+  | RecvTB n ts σs => UnitT
+  | EndBT => UnitT
+  end.
+
+Lemma session_type_equiv_alt (σ1 σ2 : session_type) :
+  session_type_id σ1 ≡ session_type_id σ2 -> σ1 ≡ σ2.
+Proof.
+  intros.
+  rewrite -(session_type_id_id σ1).
+  rewrite -(session_type_id_id σ2).
+  done.
+Defined.
+
+Lemma dual_dual σ : dual (dual σ) = σ.
+Proof.
+  apply session_type_extensionality.
+  apply session_type_equiv_alt.
+  revert σ. cofix IH. intros []; simpl; constructor; try done; intros i;
+  apply session_type_equiv_alt; apply IH.
+Qed.
+
+Lemma toL_toL1 σ :
+  toL σ = FunT Lin (toL1 σ) (toL1 (dual σ)).
+Proof.
+  rewrite -{1}(type_id_id (toL _)).
+  destruct σ; simpl; eauto; simplify_eq.
+  f_equal. f_equal. apply functional_extensionality.
+  intros x. f_equal. rewrite dual_dual. done.
+Qed.
+
 Lemma toL_toL_dual_split σ :
   ∃ t1 t2, toL σ = FunT Lin t1 t2 ∧ toL (dual σ) = FunT Lin t2 t1.
 Proof.
-
-Admitted.
+  exists (toL1 σ), (toL1 (dual σ)).
+  rewrite toL_toL1 //. split; eauto.
+  rewrite toL_toL1 dual_dual //.
+Qed.
 
 (* Prove typing rule for ForkB admissible *)
 
@@ -182,5 +238,6 @@ Lemma CloseB_typed Γ e :
   typed Γ (CloseB e) UnitT.
 Proof.
   intros H. rewrite /CloseB.
-  rewrite -(type_id_id (toL _)) // in H.
+  rewrite -(type_id_id (toL _)) // in H. simpl in *.
+  eapply App_typed; eauto using env_split_left, typed, env_unr_empty.
 Qed.
